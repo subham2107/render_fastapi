6468afc940f93ec9af16f503e042d8afa1e42df8
Subham Mitra <subham.mitra21@gmail.com>
2024-06-21 19:32:52 +0530

    changed validation

--- main.py
+++ main.py
@@ -20,14 +20,15 @@ async def handle_event(request: Request):
         events = await request.json()
         for event in events:
             # Process each event
-              if event.get('eventType') == 'Microsoft.EventGrid.SubscriptionValidationEvent':
-                validation_code = event['data']['validationCode']
-                validation_response = {
-                    "validationResponse": validation_code
-                }
-                logging.info(f"Received subscription validation event. Validation code: {validation_code}")
+            #   if event.get('eventType') == 'Microsoft.EventGrid.SubscriptionValidationEvent':
+            #     validation_code = event['data']['validationCode']
+            #     validation_response = {
+            #         "validationResponse": validation_code
+            #     }
+            #     logging.info(f"Received subscription validation event. Validation code: {validation_code}")
+                logging.info(f"Received event: {event}")
                 print(f"Received event: {event}")
-                return JSONResponse(content=validation_response, status_code=200)
+                # return JSONResponse(content=validation_response, status_code=200)
                 
 
         return JSONResponse({"status": "success"}, status_code=200)

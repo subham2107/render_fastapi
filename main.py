a590a997ada914f6f19a699d829049b7259314b0
Subham Mitra <46347405+subham2107@users.noreply.github.com>
2024-06-23 20:12:53 +0530

    added import

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
 import httpx
 
